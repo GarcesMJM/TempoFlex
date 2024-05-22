89b3bb2a583fab148180760ff00583c9c01955c1
Borregonorrea <samuel.orregoc@upb.edu.co>
2024-05-22 18:46:44 -0500

    Creacion de los archivos para la pagina de perfil

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './css/App.css';
 //import Profile from './views/Profile';
 import Home from './views/Home';
 import Login from './views/Login';
+import Perfil from './views/Perfil';
 //import ForgotPwd from './views/ForgotPwd';
 import { Routes, Route } from "react-router-dom";
 
@@ -12,6 +13,8 @@ function App() {
       <Route path="/" element={<Home />} />
       
       <Route path="login" element={<Login />} />
+
+      <Route path="perfil" element={<Perfil />} />
       
     </Routes>
   );
---
+++ frontend/src/views/Perfil.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../css/Perfil.css";
+
+import Swal from "sweetalert2";
+
+
+
+
+const Perfil = () => {
+    //AQUI FUNCIONALIDAD
+
+
+    return(
+
+    );
+};
